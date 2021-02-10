9056b3f8c680c95e0d960ce6c1712aa6c1cd5fcd
Jim Browning <jamalebrowning@gmail.com>
2021-02-09 19:38:06 -0600

    Filter products by min_price

--- bangazonapi/views/product.py
+++ bangazonapi/views/product.py
@@ -249,6 +249,7 @@ class Products(ViewSet):
         direction = self.request.query_params.get('direction', None)
         number_sold = self.request.query_params.get('number_sold', None)
         location = self.request.query_params.get('location', None)
+        min_price = self.request.query_params.get('min_price', None )
 
         if order is not None:
             order_filter = order
@@ -264,6 +265,9 @@ class Products(ViewSet):
 
         if quantity is not None:
             products = products.order_by("-created_date")[:int(quantity)]
+        
+        if min_price is not None:
+            products = products.filter(price__gt=min_price)
         
         if location is not None:
             products = products.filter(location__contains=location)
