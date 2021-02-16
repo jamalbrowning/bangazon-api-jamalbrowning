a87502dee577aedca17383134fa77f829acfc971
Jim Browning <jamalebrowning@gmail.com>
2021-02-15 21:06:24 -0600

    Test Delete Product is now working.

--- tests/product.py
+++ tests/product.py
@@ -96,5 +96,26 @@ class ProductTests(APITestCase):
         self.assertEqual(len(json_response), 3)
 
     # TODO: Delete product
+    def test_delete_product(self):
+        """
+        Ensure we can remove a product.
+        """
+        # add product
+        self.test_get_all_products()
 
+        # remove product
+        url = "/products/1"
+        data = {"id": 1}
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
+        response = self.client.delete(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        # get products and verify product was removed
+        url = "/products"
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
+        response = self.client.get(url, None, format='json')
+        json_response = json.loads(response.content)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
     # TODO: Product can be rated. Assert average rating exists.
