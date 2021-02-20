997e66f917a3554023d59301e47fa31a7b189802
Jim Browning <jamalebrowning@gmail.com>
2021-02-20 09:32:58 -0600

    changed sql for price

--- reports/views/products/inexpensiveproducts.py
+++ reports/views/products/inexpensiveproducts.py
@@ -17,7 +17,7 @@ def inexpensiveproduct_list(request):
               SELECT 
                 *
               FROM bangazonapi_product
-              WHERE bangazonapi_product.price <= 999 
+              WHERE bangazonapi_product.price <= 999.99 
               ORDER BY bangazonapi_product.price ASC
         """)
 
