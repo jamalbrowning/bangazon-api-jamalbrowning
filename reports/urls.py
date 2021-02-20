ca02f9e4d8af3502f9e3ba8f57490055632df5d0
Jim Browning <jamalebrowning@gmail.com>
2021-02-20 09:04:42 -0600

    Made report for inexpensive products

--- reports/urls.py
+++ reports/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import expensiveproduct_list
+from .views import expensiveproduct_list, inexpensiveproduct_list
 
 urlpatterns = [
-    path('reports/expensiveproducts', expensiveproduct_list)
+    path('reports/expensiveproducts', expensiveproduct_list),
+    path('reports/inexpensiveproducts', inexpensiveproduct_list)
 ]
