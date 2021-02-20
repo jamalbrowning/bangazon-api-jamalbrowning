a58bd4cca0c90dfc6b4e1e6ba1f6234d7ac13c63
Jim Browning <jamalebrowning@gmail.com>
2021-02-20 13:06:54 -0600

    completed orders done

--- reports/urls.py
+++ reports/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import expensiveproduct_list, inexpensiveproduct_list
+from .views import expensiveproduct_list, inexpensiveproduct_list, completedorder_list
 
 urlpatterns = [
     path('reports/expensiveproducts', expensiveproduct_list),
-    path('reports/inexpensiveproducts', inexpensiveproduct_list)
+    path('reports/inexpensiveproducts', inexpensiveproduct_list),
+    path('reports/completedorders', completedorder_list)
 ]
--- reports/views/__init__.py
+++ reports/views/__init__.py
@@ -1,3 +1,4 @@
 from .connection import Connection
 from .products.expensiveproducts import expensiveproduct_list
 from .products.inexpensiveproducts import inexpensiveproduct_list
+from .orders.completedorders import completedorder_list
